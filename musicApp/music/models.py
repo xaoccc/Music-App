e540cb6732371a28bd8f1fb347999db18703a5c8
xaoccc <xaocccc@gmail.com>
2023-11-23 11:38:07 +0200

    setup index.html url and view - success!

--- musicApp/music/models.py
+++ musicApp/music/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    image_url = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Song(models.Model):
+    name = models.CharField()
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
